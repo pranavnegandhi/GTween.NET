06fd90fac57521eda82d11abece2ec171702433e
Pranav Negandhi <pranav@notadesigner.com>
2019-07-05 14:21:31 +0530

    #7 - The names of the easing methods in the Circular class do not follow the C# language naming convention

--- src/GTween/Motion/Easing/Circular.cs
+++ src/GTween/Motion/Easing/Circular.cs
@@ -14,17 +14,17 @@ namespace GSkinner.Motion.Easing
     public class Circular
     {
         // unused params are included for compatibility with other easing classes.
-        public static double easeIn(double ratio, double unused1, double unused2, double unused3)
+        public static double EaseIn(double ratio, double unused1, double unused2, double unused3)
         {
             return -(Math.Sqrt(1 - ratio * ratio) - 1);
         }
 
-        public static double easeOut(double ratio, double unused1, double unused2, double unused3)
+        public static double EaseOut(double ratio, double unused1, double unused2, double unused3)
         {
             return Math.Sqrt(1 - (ratio - 1) * (ratio - 1));
         }
 
-        public static double easeInOut(double ratio, double unused1, double unused2, double unused3)
+        public static double EaseInOut(double ratio, double unused1, double unused2, double unused3)
         {
             return ((ratio *= 2) < 1) ? -0.5 * (Math.Sqrt(1 - ratio * ratio) - 1) : 0.5 * (Math.Sqrt(1 - (ratio -= 2) * ratio) + 1);
         }
