b877df76a15bd53cbf570811c7dd432c2cad2c53
Pranav Negandhi <pranav@notadesigner.com>
2019-07-07 19:16:40 +0530

    #9 - Improve performance of the set-property operation
    Added a LoadTestingWindow class to test performance of the library against large loads
    To use this window, replace the instantiation of MainWindow in Program.cs with LoadTestingWindow

---
+++ src/EasingDemos/LoadTestingWindow.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace EasingDemos
+{
+    partial class LoadTestingWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instanceCounter = new System.Windows.Forms.NumericUpDown();
+            this.numberOfInstances = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.bitmapDisplay = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.instanceCounter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bitmapDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // instanceCounter
+            // 
+            this.instanceCounter.Location = new System.Drawing.Point(123, 12);
+            this.instanceCounter.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.instanceCounter.Name = "instanceCounter";
+            this.instanceCounter.Size = new System.Drawing.Size(149, 20);
+            this.instanceCounter.TabIndex = 0;
+            this.instanceCounter.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numberOfInstances
+            // 
+            this.numberOfInstances.AutoSize = true;
+            this.numberOfInstances.Location = new System.Drawing.Point(12, 14);
+            this.numberOfInstances.Name = "numberOfInstances";
+            this.numberOfInstances.Size = new System.Drawing.Size(105, 13);
+            this.numberOfInstances.TabIndex = 1;
+            this.numberOfInstances.Text = "&Number of Instances";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(197, 38);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 2;
+            this.start.Text = "&Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // bitmapDisplay
+            // 
+            this.bitmapDisplay.Location = new System.Drawing.Point(12, 67);
+            this.bitmapDisplay.Name = "bitmapDisplay";
+            this.bitmapDisplay.Size = new System.Drawing.Size(260, 882);
+            this.bitmapDisplay.TabIndex = 3;
+            this.bitmapDisplay.TabStop = false;
+            // 
+            // LoadTestingWindow
+            // 
+            this.AcceptButton = this.start;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 961);
+            this.Controls.Add(this.bitmapDisplay);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.numberOfInstances);
+            this.Controls.Add(this.instanceCounter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoadTestingWindow";
+            this.Text = "GTween Load Testing";
+            this.Load += new System.EventHandler(this.LoadTestingWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.instanceCounter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bitmapDisplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown instanceCounter;
+        private System.Windows.Forms.Label numberOfInstances;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.PictureBox bitmapDisplay;
+    }
+}
---
+++ src/EasingDemos/LoadTestingWindow.cs
@@ -0,0 +1,125 @@
+﻿using GSkinner.Motion;
+using GSkinner.Motion.Easing;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EasingDemos
+{
+    public partial class LoadTestingWindow : Form
+    {
+        private GTween[] _tweens;
+
+        private Bitmap _bitmap;
+
+        private readonly object _bitmapDisplayPadlock = new { };
+
+        private Graphics _graphics;
+
+        private int _maxWidth;
+
+        private int _maxHeight;
+
+        private readonly Timer _refresh = new Timer();
+
+        private int _completedCounter;
+
+        public LoadTestingWindow()
+        {
+            InitializeComponent();
+
+            DoubleBuffered = true;
+
+            _refresh.Interval = 40;
+            _refresh.Tick += (s, e) => bitmapDisplay.Invalidate();
+            _refresh.Enabled = false;
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            _graphics.FillRectangle(Brushes.Black, 0, 0, _maxWidth, _maxHeight);
+
+            var desiredCount = (int)instanceCounter.Value;
+            _completedCounter = 0;
+
+            _tweens = new GTween[desiredCount];
+            GTween.PauseAll = true;
+            for (var i = 0; i < desiredCount; i++)
+            {
+                var initializer = new GTweenPropertyInitializer()
+                {
+                    AutoPlay = false,
+                    DispatchEvents = true,
+                    SuppressEvents = false
+                };
+
+                var target = new Point(0, i * 10);
+                var values = new Dictionary<string, double>() { { "X", _maxWidth }, { "Y", _maxHeight + target.Y } };
+                var tween = new GTween(target, 5, values, initializer);
+                tween.Data = new Point(0, i * 10);
+                tween.Ease = Sine.EaseInOut;
+                tween.Completed += TweenCompleted;
+                tween.Paused = false;
+                _tweens[i] = tween;
+            }
+            GTween.PauseAll = false;
+
+            _refresh.Enabled = true;
+        }
+
+        private void TweenCompleted(object sender, GTweenEventArgs e)
+        {
+            var tween = e.Tween;
+            tween.Completed -= TweenCompleted;
+            _completedCounter++;
+
+            if (_refresh.Enabled && _completedCounter == _tweens.Length)
+            {
+                _refresh.Enabled = false;
+            }
+        }
+
+        private void LoadTestingWindow_Load(object sender, EventArgs e)
+        {
+            _maxWidth = bitmapDisplay.Width - 1;
+            _maxHeight = bitmapDisplay.Height - 1;
+
+            _bitmap = new Bitmap(bitmapDisplay.Width, bitmapDisplay.Height);
+            _graphics = Graphics.FromImage(_bitmap);
+            _graphics.FillRectangle(Brushes.Black, 0, 0, _maxWidth, _maxHeight);
+            bitmapDisplay.Image = _bitmap;
+
+            bitmapDisplay.Paint += BitmapDisplay_Paint;
+        }
+
+        private void BitmapDisplay_Paint(object sender, PaintEventArgs e)
+        {
+            if (null != _tweens)
+            {
+                var count = _tweens.Length;
+
+                for (var i = 0; i < count; i++)
+                {
+                    var tween = _tweens[i];
+                    var from = (Point)tween.Data;
+                    var to = (Point)tween.Target;
+
+                    var yTo = Math.Max((float)to.Y, 0f);
+                    yTo = Math.Min(yTo, _maxHeight);
+
+                    var xFrom = Math.Max((float)from.X, 0f);
+                    xFrom = Math.Min(xFrom, _maxWidth);
+
+                    var yFrom = Math.Max((float)from.Y, 0f);
+                    yFrom = Math.Min(yFrom, _maxHeight);
+
+                    _graphics.DrawLine(Pens.Red, xFrom, yFrom, xFrom, yTo);
+
+                    from.X += _maxWidth / (5000 / 40);
+                    from.Y = to.Y;
+                }
+            }
+        }
+    }
+}
