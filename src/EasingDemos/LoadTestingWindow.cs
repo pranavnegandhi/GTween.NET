531fdcba0fbcf73c8c90258c3f2ce3e3b811ea28
Pranav Negandhi <pranav@notadesigner.com>
2019-11-17 07:05:02 +0530

    Several modifications in LoadTestingWindow to improve thread-safety. Mainly done by switching from a simple integer counter to keep track of completed tweens to using a CountdownEvent instance.

--- src/EasingDemos/LoadTestingWindow.cs
+++ src/EasingDemos/LoadTestingWindow.cs
@@ -3,6 +3,7 @@ using GSkinner.Motion.Easing;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace EasingDemos
@@ -21,9 +22,9 @@ namespace EasingDemos
 
         private int _maxHeight;
 
-        private readonly Timer _refresh = new Timer();
+        private readonly System.Windows.Forms.Timer _refresh = new System.Windows.Forms.Timer();
 
-        private int _completedCounter;
+        private CountdownEvent _counter;
 
         public LoadTestingWindow()
         {
@@ -41,7 +42,7 @@ namespace EasingDemos
             _graphics.FillRectangle(Brushes.Black, 0, 0, _maxWidth, _maxHeight);
 
             var desiredCount = (int)instanceCounter.Value;
-            _completedCounter = 0;
+            _counter = new CountdownEvent(desiredCount);
 
             _tweens = new GTween[desiredCount];
             GTween.PauseAll = true;
@@ -50,13 +51,13 @@ namespace EasingDemos
                 var initializer = new GTweenPropertyInitializer()
                 {
                     AutoPlay = false,
-                    DispatchEvents = true
+                    DispatchEvents = true,
+                    Data = new Point(0, i * 5)
                 };
 
-                var target = new Point(0, i * 10);
-                var values = new Dictionary<string, double>() { { "X", _maxWidth }, { "Y", _maxHeight + target.Y } };
+                var target = new Point(0, i * 5);
+                var values = new Dictionary<string, double>() { { "Y", _maxHeight } };
                 var tween = new GTween(target, 5, values, initializer);
-                tween.Data = new Point(0, i * 10);
                 tween.Ease = Sine.EaseInOut;
                 tween.Completed += TweenCompleted;
                 tween.Paused = false;
@@ -71,11 +72,39 @@ namespace EasingDemos
         {
             var tween = e.Tween;
             tween.Completed -= TweenCompleted;
-            _completedCounter++;
 
-            if (_refresh.Enabled && _completedCounter == _tweens.Length)
+            var from = (Point)tween.Data;
+            var to = (Point)tween.Target;
+
+            var xFrom = Math.Max((float)from.X, 0f);
+            xFrom = Math.Min(xFrom, _maxWidth);
+
+            var yFrom = Math.Max((float)from.Y, 0f);
+            yFrom = Math.Min(yFrom, _maxHeight);
+
+            var xTo = xFrom;
+            xTo += _maxWidth / ((float)tween.Duration * 1000 / 40);
+            xTo = Math.Max(xTo, 0.0f);
+            xTo = Math.Min(xTo, _maxWidth);
+
+            var yTo = Math.Max((float)to.Y, 0f);
+            yTo = Math.Min(yTo, _maxHeight);
+
+            try
             {
-                _refresh.Enabled = false;
+                _graphics.DrawLine(Pens.Bisque, xFrom, yFrom, xTo, yTo);
+            }
+            catch (InvalidOperationException)
+            {
+            }
+            finally
+            {
+                _counter.Signal();
+
+                if (_refresh.Enabled && _counter.IsSet)
+                {
+                    _refresh.Enabled = false;
+                }
             }
         }
 
@@ -101,11 +130,9 @@ namespace EasingDemos
                 for (var i = 0; i < count; i++)
                 {
                     var tween = _tweens[i];
+
                     var from = (Point)tween.Data;
                     var to = (Point)tween.Target;
-
-                    var yTo = Math.Max((float)to.Y, 0f);
-                    yTo = Math.Min(yTo, _maxHeight);
 
                     var xFrom = Math.Max((float)from.X, 0f);
                     xFrom = Math.Min(xFrom, _maxWidth);
@@ -113,10 +140,24 @@ namespace EasingDemos
                     var yFrom = Math.Max((float)from.Y, 0f);
                     yFrom = Math.Min(yFrom, _maxHeight);
 
-                    _graphics.DrawLine(Pens.Red, xFrom, yFrom, xFrom, yTo);
+                    var xTo = xFrom;
+                    xTo += _maxWidth / ((float)tween.Duration * 1000 / 40);
+                    xTo = Math.Max(xTo, 0.0f);
+                    xTo = Math.Min(xTo, _maxWidth);
 
-                    from.X += _maxWidth / (5000 / 40);
+                    var yTo = Math.Max((float)to.Y, 0f);
+                    yTo = Math.Min(yTo, _maxHeight);
+
+                    from.X = xTo;
                     from.Y = to.Y;
+
+                    try
+                    {
+                        _graphics.DrawLine(Pens.Bisque, xFrom, yFrom, xTo, yTo);
+                    }
+                    catch (InvalidOperationException)
+                    {
+                    }
                 }
             }
         }
